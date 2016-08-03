8b8295c2d3d99c54f38cb9f65e8c840dde15d38f
David Gwartney <davidg@boundary.com>
2016-08-03 07:15:08 -0700

    Put logic into class

--- soup.py
+++ soup.py
@@ -4,40 +4,51 @@ from urlparse import urlparse
 from urllib import urlopen
 from bs4 import BeautifulSoup
 from time import time
-url = 'http://web-1/index.html'
-url = 'http://www.bmc.com'
-start_url = urlparse(url)
-html = urlopen(url)
-bsObj = BeautifulSoup(html.read(), 'html.parser')
+import sys
+import logging
 
-def worker(img):
+
+logger = logging.getLogger(__name__)
+
+
+def worker(img, url):
     """
     thread worker function
     """
     src = img.get('src')
     o = urlparse(src)
-    print(o)
-    print(type(0))
     if len(o.netloc) == 0:
-        src = start_url.scheme + '://' + start_url.netloc + '/' + src
+        src = url.scheme + '://' + url.netloc + '/' + src
     html = urlopen(src)
     data = html.read()
-    print(html.geturl())
+    logger.debug(html.geturl())
     return
 
+
 class Soup(object):
-
     def __init__(self, url):
-	self.url = url
+        self.url = url
 
-threads=[]
-time_begin = time()
-for img in bsObj.findAll('img'):
-    t = threading.Thread(target=worker, args=(img,))
-    threads.append(t)
-    t.start()
+    def measure_load_time(self):
+        time_begin = time()
+        start_url = urlparse(self.url)
+        html = urlopen(self.url)
+        bsObj = BeautifulSoup(html.read(), 'html.parser')
+        threads = []
+        for img in bsObj.findAll('img'):
+            t = threading.Thread(target=worker, args=(img, start_url))
+            threads.append(t)
+            t.start()
 
-for thread in threads:
-    thread.join()
-time_end = time()
-print(time_end - time_begin)
+        for thread in threads:
+            thread.join()
+        time_end = time()
+        return time_end - time_begin
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write("usage: {0} url\n".format(sys.argv[0]))
+        exit(1)
+    soup = Soup(url=sys.argv[1])
+    print(soup.measure_load_time())
